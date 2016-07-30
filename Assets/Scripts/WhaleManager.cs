f0ba5affadaff79e9f651faa9e089c2aabdc64d2
Kevin Calderone <bovinedragon@gmail.com>
2016-07-30 11:53:00 -0700

    Less whales/boats. Fixed bug where whales would spawn on land.

--- Assets/Scripts/WhaleManager.cs
+++ Assets/Scripts/WhaleManager.cs
@@ -7,7 +7,8 @@ public class WhaleManager : MonoBehaviour {
 	public GameObject m_Whale;
 	public TerrainBuilder m_TerrainBuilder;
 
-	private const int c_num_whales = 5;
+	public int m_num_whales = 3;
+
 	private const int c_whale_y = 1;
 	private const int c_map_width = 180;
 	private const int c_map_height = 130;
@@ -45,13 +46,13 @@ public class WhaleManager : MonoBehaviour {
     }
 
 	void SpawnWhales () {
-		const float minDist = 20.0f;
+		const float minDist = 40.0f;
 		const float minDistSqr = minDist * minDist;
 
-		if (m_TerrainBuilder == null || m_TerrainBuilder.HeightDataReady() || m_whaleList.Count >= c_num_whales)
+		if (m_TerrainBuilder == null || !m_TerrainBuilder.HeightDataReady() || m_whaleList.Count >= m_num_whales)
 			return;
 
-		for (int i = 0; i < c_num_whales; ++i) {
+		for (int i = 0; i < m_num_whales; ++i) {
 			Vector3 pos;
 			do {
 				pos = new Vector3(
